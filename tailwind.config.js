0d62d68743d82fe3495cf6f6549f7ba79d7a62a3
Iva Agarwal <137194819+iva-agarwal@users.noreply.github.com>
2024-04-11 21:04:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./src/**/*.{html,js}"];
+export const theme = {
+
+  extend: {
+    fontFamily:{
+      rough:["Aberforth Rough","sans-serif"],
+      outline:["Aberforth outline","sans-serif"],
+      demo:["Aberforth demo","sans-serif"]
+    }
+  },
+};
+export const plugins = [];
+
